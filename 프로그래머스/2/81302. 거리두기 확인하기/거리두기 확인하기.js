a6db54fa9208620ddce6cb0471719b11cca64bcb
권경민 <phillip0919@gmail.com>
2024-11-11 17:51:38 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.46 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
@@ -1,68 +1,54 @@
 function solution(places) {
-  const result = [];
-  for (const place of places) {
-    let isSafePlace = 1;
-
-    // 대기실을 순회하며 응시자의 자리에 대해 거리두기를 확인
-    for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 5; j++) {
-        // 응시자가 앉아있는데
-        if (place[i][j] === "P") {
-          // 거리두기를 지키지 않으면
-          if (!isSafe(place, i, j)) {
-            isSafePlace = 0;
-            break;
-          }
-        }
-      }
-
-      if (!isSafePlace) {
-        break;
-      }
-    }
-
-    result.push(isSafePlace);
-  }
-
-  return result;
-}
-
-function isSafe(place, x, y) {
-  // 상하좌우 방향을 나타내는 배열
-  const dx = [0, 0, 1, -1];
-  const dy = [1, -1, 0, 0];
-
-  for (let i = 0; i < 4; i++) {
-    // 인접한 위치 좌표 계산
-    const nx = x + dx[i];
-    const ny = y + dy[i];
-
-    // 대기실 범위 이내 여부 확인
-    if (nx >= 0 && nx < 5 && ny >= 0 && ny < 5) {
-      // 맨해튼 거리가 1인 경우
-      if (place[nx][ny] === "P") {
-        return false;
-        // 인접한 위치에 빈 테이블이 있는 경우
-      } else if (place[nx][ny] === "O") {
-        for (let j = 0; j < 4; j++) {
-          // 빈 테이블을 기준으로 다른 방향으로 인접한 위치 좌표 계산
-          const nnx = nx + dx[j];
-          const nny = ny + dy[j];
-
-          if (nnx >= 0 && nnx < 5 && nny >= 0 && nny < 5) {
-            // 현재 응시자의 위치가 아닌 경우
-            if (nnx !== x || nny !== y) {
-              // 맨해튼 거리가 2이고 파티션 없이 응시자가 있는 경우
-              if (place[nnx][nny] === "P") {
-                return false;
-              }
+    const dx = [0, 0, -1, 1]
+    const dy = [-1, 1, 0, 0]
+    var answer = [];
+    
+    const pLocation = Array.from({length: places.length}, () => [])
+    
+    for (let p = 0; p < places.length; p++) {
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 5; j++) {
+                if (places[p][i][j] === "P") pLocation[p].push([i, j])
             }
-          }
-        }
-      }
+        }    
     }
-  }
+    
+    for (let p = 0; p < places.length; p++) {
+        const map = places[p]
+        const queue = [...pLocation[p]]
+        let flag = 1
 
-  // 거리두기를 모두 지키면 true
-  return true;
+        while(queue.length > 0) {
+            const [x, y] = queue.shift()
+            for (let i = 0; i < 4; i++) {
+                const nx = x + dx[i]
+                const ny = y + dy[i]
+                
+                if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5) {
+                    if (map[nx][ny] === "P") {
+                        flag = 0
+                        break;    
+                    } else if (map[nx][ny] === "O") {
+                        for (let j = 0; j < 4; j++) {
+                            const nnx = nx + dx[j]
+                            const nny = ny + dy[j]
+                            
+                            if (nnx >= 0 && nny >= 0 && nnx < 5 && nny < 5 && map[nnx][nny] === "P") {
+                                if (nnx != x || nny != y) {
+                                    flag = 0
+                                    break    
+                                }
+                            }
+                        }
+                    }
+                }
+                if (!flag) break
+            }    
+            if (!flag) break
+        }
+        
+        answer.push(flag)
+    }
+    
+    return answer;
 }
