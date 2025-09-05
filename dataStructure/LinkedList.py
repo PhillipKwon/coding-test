7b805c487bc5a978f63d2410f38c9e06b62b2f96
PhillipKwon <phillip0919@gmail.com>
2025-09-05 11:22:04 +0900

    refactor: LinkedList Refactor

--- dataStructure/LinkedList.py
+++ dataStructure/LinkedList.py
@@ -8,53 +8,55 @@ class LinkedList:
         self.head = None
     
     def append(self, val):
-        # 새로운 노드를 리스트 끝에 추가
-        # TODO: 여기를 구현해보세요
         newNode = ListNode(val)
-        if (self.head is None):
-            self.head = newNode
-            return
-        last = self.head
-        while (last.next):
-            last = last.next
-        last.next = newNode
+        if self.head is None:
+          self.head = newNode
+          return
+        
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = newNode
 
     def prepend(self, val):
-        # 새로운 노드를 리스트 앞에 추가
-        # TODO: 여기를 구현해보세요
-        newNode = LinkedList(val)
+        newNode = ListNode(val)
         newNode.next = self.head
         self.head = newNode
 
     def display(self):
-        # 리스트의 모든 값을 출력
-        # TODO: 여기를 구현해보세요
         current = self.head
-        while current:
-            print(current.val, end=" -> ")
+        while current is not None:
+            print(current.val, " -> ", end="")
             current = current.next
+
         print("None")
-    
+
     def delete(self, val):
-        # 특정 값을 가진 노드 삭제
-        # TODO: 여기를 구현해보세요
+        if self.head is None:
+            return
+        
+        if self.head.val == val:
+            self.head = self.head.next
+            return
+        
+        prev = self.head
         current = self.head
-        while current:
+        while current is not None:
             if current.val == val:
-                if current.prev:
-                    current.prev.next = current.next
-                if current.next:
-                    current.next.prev = current.prev
-                if current == self.head:
-                    self.head = current.next
+                prev.next = current.next
                 return
+            prev = current
             current = current.next
 
 # 테스트 코드
 if __name__ == "__main__":
     ll = LinkedList()
-    ll.append(1)
-    ll.append(2) 
     ll.append(3)
-    ll.display()  # 출력: 1 -> 2 -> 3 -> None
+    ll.prepend(2)
+    ll.append(4)
+    ll.prepend(1)
+    ll.append(5)
+    ll.display()
+    ll.delete(5)
+    ll.display()
 
