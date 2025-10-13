e91df338bb5c464ccd576b12f462f841587fb604
PhillipKwon <phillip0919@gmail.com>
2025-10-13 16:59:11 +0900

    <add>: LeetCode 206 문제 풀이

---
+++ leetcode/easy/206. Reverse Linked List.ts
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let prev: ListNode | null = null;
+  let current = head;
+
+  while (current) {
+    let next = current.next;
+
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+}
