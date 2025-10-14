8a7d9a2623818656e6598a2ca7d77ccaeca9c03c
PhillipKwon <phillip0919@gmail.com>
2025-10-14 17:29:33 +0900

    <add>: LeetCode 141 문제 풀이

---
+++ leetcode/easy/141. Linked List Cycle.ts
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow!.next;
+    fast = fast!.next!.next;
+    if (fast === slow) return true;
+  }
+
+  return false;
+}
