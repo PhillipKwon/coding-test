ca28fbf8dd731eadc674564051122eeb0affe3cd
PhillipKwon <phillip0919@gmail.com>
2025-10-21 10:56:42 +0900

    <add>: LeetCode 704 문제 풀이

---
+++ leetcode/easy/704. Binary Search.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/binary-search/
+
+function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
